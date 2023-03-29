c3c7c8ba44f0b712ed0579e5b68b26a42aa850b0
xiangyong <patience404@163.com>
2023-03-29 14:41:44 +0800

    Initial commit

---
+++ src/test/java/com/suancaiyu/forfun/ForfunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suancaiyu.forfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForfunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
